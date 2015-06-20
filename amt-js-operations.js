2df292bc9d91a74a10de4bafc4dbe205de5d31ba
Dave Shumway <davidshumway@gmail.com>
2015-06-20 01:28:21 -0400

    Update amt-js-operations.js

--- amt-js-operations.js
+++ amt-js-operations.js
@@ -2,16 +2,19 @@
  *
  */
 var amt = function() {
-  
   /**
-   * function generate_signature
-   * @param options Object containing strings "operation" and "secret_key".
-   * @return Object containing keys "service", "operation", "timestamp", and "signature"
+   *
    */
-  function generate_signature(options) {
+  this.opts = ["ApproveAssignment","ApproveRejectedAssignment","AssignQualification","BlockWorker","ChangeHITTypeOfHIT","CreateHIT","CreateQualificationType","DisableHIT","DisposeHIT","DisposeQualificationType","ExtendHIT","ForceExpireHIT","GetAccountBalance","GetAssignment","GetAssignmentsForHIT","GetBlockedWorkers","GetBonusPayments","GetFileUploadURL","GetHIT","GetHITsForQualificationType","GetQualificationsForQualificationType","GetQualificationRequests","GetQualificationScore","GetQualificationType","GetReviewableHITs","GetReviewResultsForHIT","GetRequesterStatistic","GetRequesterWorkerStatistic","GrantBonus","GrantQualification","NotifyWorkers","RegisterHITType","RejectAssignment","RejectQualificationRequest","RevokeQualification","SearchHITs","SearchQualificationTypes","SendTestEventNotification","SetHITAsReviewing","SetHITTypeNotification"];
+}
+/**
+ * function generate_signature
+ * @param options Object containing strings "operation" and "secret_key".
+ * @return Object containing keys "service", "operation", "timestamp", and "signature"
+ */
+amt.prototype.generate_signature = function(options) {
     var service = "AWSMechanicalTurkRequester";
     var timestamp = new Date().toISOString();
-    var secret = key.value;
     var str = service + options.operation + timestamp;
     var signature = CryptoJS.HmacSHA1(str, options.secret_key).toString(CryptoJS.enc.Base64);
     return {
@@ -22,9 +25,3 @@ var amt = function() {
       signature: signature
     }
   }
-  
-  /**
-   *
-   */
-  var opts = ["ApproveAssignment","ApproveRejectedAssignment","AssignQualification","BlockWorker","ChangeHITTypeOfHIT","CreateHIT","CreateQualificationType","DisableHIT","DisposeHIT","DisposeQualificationType","ExtendHIT","ForceExpireHIT","GetAccountBalance","GetAssignment","GetAssignmentsForHIT","GetBlockedWorkers","GetBonusPayments","GetFileUploadURL","GetHIT","GetHITsForQualificationType","GetQualificationsForQualificationType","GetQualificationRequests","GetQualificationScore","GetQualificationType","GetReviewableHITs","GetReviewResultsForHIT","GetRequesterStatistic","GetRequesterWorkerStatistic","GrantBonus","GrantQualification","NotifyWorkers","RegisterHITType","RejectAssignment","RejectQualificationRequest","RevokeQualification","SearchHITs","SearchQualificationTypes","SendTestEventNotification","SetHITAsReviewing","SetHITTypeNotification"];
-}
