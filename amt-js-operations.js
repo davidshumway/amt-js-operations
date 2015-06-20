192442479979587f7a51b60f14fcdce8c50bd4a2
Dave Shumway <davidshumway@gmail.com>
2015-06-20 01:44:05 -0400

    Update amt-js-operations.js

--- amt-js-operations.js
+++ amt-js-operations.js
@@ -13,17 +13,27 @@ var amt = function() {
  * @return Object containing keys "service", "operation", "timestamp", and "signature"
  */
 amt.prototype.generate_signature = function(options) {
-  var service = "AWSMechanicalTurkRequester";
-  var timestamp = new Date().toISOString();
-  var str = service + options.operation + timestamp;
-  var signature = CryptoJS.HmacSHA1(str, options.secret_key).toString(CryptoJS.enc.Base64);
-  return {
+  var g = {
     operation: options.operation,
     secret_key: options.secret_key,
-    service: service,
-    timestamp: timestamp,
-    signature: signature,
-    url_encoded_timestamp: encodeURIComponent(timestamp),
+    service: "AWSMechanicalTurkRequester",
+    timestamp: null,
+    signature: null,
+    url_encoded_timestamp: null,
     url_encoded_signature: encodeURIComponent(signature),
+    stub_url: null
   }
+  g.timestamp = new Date().toISOString();
+  g.url_encoded_timestamp = encodeURIComponent(g.timestamp);
+  var s = g.service + g.operation + g.timestamp;
+  g.signature = CryptoJS.HmacSHA1(s, g.secret_key).toString(CryptoJS.enc.Base64);
+  g.url_encoded_signature = encodeURIComponent(g.signature);
+  var stub =
+    'https://mechanicalturk.amazonaws.com/?Service='+encodeURIComponent(g.service)+
+    '&Version=2014-08-15'+
+    '&Operation='+encodeURIComponent(g.operation)+
+    '&Signature='+g.url_encoded_signature+
+    '&Timestamp='+g.url_encoded_timestamp
+  ;
+  return g;
 }
